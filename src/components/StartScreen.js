5c46815776720ae7f35b5fe04a0aeafbcc3b3a14
jmaraujo93 <jmaraujo93@gmail.com>
2022-06-09 20:23:52 -0300

    first commit

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start" >
+        <h1>Secret Word</h1>
+        <p>Clique to start</p>
+        <button>START</button>
+    </div>
+  )
+}
+
+export default StartScreen
