981a28cb3f23099097871bb50a5478a8e1afd648
jmaraujo93 <jmaraujo93@gmail.com>
2022-06-10 15:22:12 -0300

    port layout

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -3,9 +3,9 @@ import './GameOver.css'
 const GameOver = ({retry, score}) => {
   return (
     <div>
-      <h1>Game Over</h1>
-      <h2>Your score: <span>{score}</span> </h2>
-      <button onClick={retry}>Retry!</button>
+      <h1>Fim de Jogo!</h1>
+      <h2>Sua pontuação: <span>{score}</span> </h2>
+      <button onClick={retry}>Tentar Novamente</button>
     </div>
   )
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,9 +3,9 @@ import './StartScreen.css'
 const StartScreen = ({startgame}) => {
   return (
     <div className="start" >
-        <h1>Secret Word</h1>
-        <p>Clique to start</p>
-        <button onClick={startgame}>START</button>
+        <h1>Palavra Secreta</h1>
+        <p>Clique para começar</p>
+        <button onClick={startgame}>COMEÇAR</button>
     </div>
   )
 }
